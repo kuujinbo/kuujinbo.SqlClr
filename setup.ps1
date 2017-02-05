f83a9608c3538809f2101704999049e3df604ca4
kuujinbo <kuujinbo@gmail.com>
2017-02-04 22:09:40 -0800

    REAME; cleanup powershell script && usage SQL script

--- setup.ps1
+++ setup.ps1
@@ -9,26 +9,21 @@ $_PROVIDER = [System.Data.Common.DbProviderFactories]::GetFactory($_PROVIDER_NAM
 function use {
     [CmdletBinding()]
     param($obj, [scriptblock]$sb)
+
     try {
-        if ($obj -is [ System.Collections.IEnumerable] -and -not ($obj -is [array])) { 
-            throw "Wrap IEnumerables in an array before passing them."
-        }
         &$sb;
     } finally {
-        if ($obj -is [IDisposable]) {             
-            $obj | foreach {
-                if ($_ -is [IDisposable]) { $_.Dispose(); }
-            }
+        if ($obj -is [IDisposable]) { 
+            $obj.Dispose();
         }
     }
 }
 
+Write-Host "creating [$_DATABASE] database";
 # create DB
 use ($c = $_PROVIDER.CreateConnection()) {
     $c.ConnectionString = "Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;";
     $c.Open();
-    Write-Host $c.database;
-
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @"
 CREATE DATABASE $_DATABASE  
@@ -41,40 +36,49 @@ ON
 LOG ON  
 ( NAME = ImdbMovies_log,  
     FILENAME = '$_SCRIPT_PATH\$($_DATABASE)_log.ldf',  
-    SIZE = 20MB,  
+    SIZE = 100MB,  
     MAXSIZE = 1024MB,  
     FILEGROWTH = 10% );
 "@;
-        $cmd.ExecuteNonQuery();
+        $cmd.ExecuteNonQuery() | Out-Null;
     }
 }
+Write-Host 'done';
 
-# setup
+# setup && config 
 use ($c = $_PROVIDER.CreateConnection()) {
     $c.ConnectionString = $_CS;
     $c.Open();
+
     # create table
+    Write-Host 'creating [movie] table';
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @'
 CREATE TABLE dbo.movies (
   title nvarchar(512) NOT NULL
 );
-'@;
+'@;  
         $cmd.ExecuteNonQuery() | Out-Null;
     }
+    Write-Host 'done';
+
     # enable CLR
+    Write-Host 'enabling database CLR'
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandType = [System.Data.CommandType]::StoredProcedure;
         $cmd.CommandText = 'sp_configure';
-        $cmd.Parameters.Add($cmd.CreateParameter());
+        $cmd.Parameters.Add($cmd.CreateParameter()) | Out-Null;
         $cmd.Parameters[0].ParameterName = '@configname';
         $cmd.Parameters[0].Value = 'clr enabled';
-        $cmd.Parameters.Add($cmd.CreateParameter());
+        $cmd.Parameters.Add($cmd.CreateParameter()) | Out-Null;
         $cmd.Parameters[1].ParameterName = '@configvalue';
         $cmd.Parameters[1].Value = 1;
         $cmd.ExecuteNonQuery() | Out-Null;;
     }
+    Write-Host 'done';
+
     # import data
+    Write-Host 'importing IMDB movie title data';
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @"
 BULK INSERT movies
@@ -83,8 +87,10 @@ WITH (ROWTERMINATOR = '\n');
 "@;
         $cmd.ExecuteNonQuery() | Out-Null;
     }
+    Write-Host 'done';
 
     # create assembly
+    Write-Host 'creating CLR assembly';
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @"
 DECLARE @dll_path varchar(8000);
@@ -96,7 +102,9 @@ EXEC('CREATE ASSEMBLY ClrUDF FROM '
 "@;
         $cmd.ExecuteNonQuery() | Out-Null;
     }
+
     # UDF => RegexMatch
+    Write-Host 'creating RegexMatch UDF';
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @'
 CREATE FUNCTION RegexMatch(
@@ -115,7 +123,10 @@ EXTERNAL NAME ClrUDF.[kuujinbo.SqlClr.UDF].RegexMatch
 '@;
         $cmd.ExecuteNonQuery() | Out-Null;
     }
+    Write-Host 'done';
+
     # UDF => RegexReplace
+    Write-Host 'creating RegexReplace UDF';
     use ($cmd = $c.CreateCommand()) {
         $cmd.CommandText = @'
 CREATE FUNCTION RegexReplace(
@@ -135,4 +146,5 @@ EXTERNAL NAME ClrUDF.[kuujinbo.SqlClr.UDF].RegexReplace
 '@;
         $cmd.ExecuteNonQuery() | Out-Null;
     }
+    Write-Host 'done';
 }
--- usage.sql
+++ usage.sql
@@ -1,39 +1,17 @@
-BULK
-INSERT movies
-FROM 'D:\downloads\imdb\movies.txt'
-WITH
-(
-   ROWTERMINATOR = '\n'
-)
-GO
-
-
 DECLARE @pattern NVARCHAR(4000);
-SET @pattern = '(?ix)^Ar.*?\bken\b.*';
+SET @pattern = '(?x)^Ar .*? \bken\b .*';
 
--- simple SELECT that the LIKE operator cannot handle
-select * from movies
-where dbo.RegexMatch(title, @pattern)=1;
+-- simple SELECT that the LIKE operator cannot handle;
+-- regular expressions are case-sensitive by default
+SELECT * FROM ImdbMovies.dbo.movies
+WHERE ImdbMovies.dbo.RegexMatch(title, @pattern)=1;
 -- test before UPDATE
-select dbo.RegexReplace(
-    title, '^', '__XXX__'
+SELECT ImdbMovies.dbo.RegexReplace(
+    title, '\b(ken)\b', '__XXX__${1}'
 )
-from movies
-where dbo.RegexMatch(title, @pattern)=1;
+FROM ImdbMovies.dbo.movies
+WHERE ImdbMovies.dbo.RegexMatch(title, @pattern)=1;
 
-
---update Customers
---set Region=dbo.RegexReplace(
---    Region, '^', '_'
---);
---select Region from Customers;
-
---update Customers
---set Region=dbo.RegexReplace(
---    Region, '^_', ''
---);
---select Region from Customers;
-
-
-select * from movies
-where title like 'Ar%ken%'
+-- LIKE operator not case-sensitive, no 'word boundary', and no capture grouping
+SELECT * FROM ImdbMovies.dbo.movies
+WHERE title like 'ar%ken%'
